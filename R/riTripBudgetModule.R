fba8db6bcc635e93b1aad2b1b617217a56d93970
BWOlatunji <bilqiiswunmi@gmail.com>
2024-02-20 10:29:17 +0100

    added trip budget module

---
+++ R/riTripBudgetModule.R
@@ -0,0 +1,15 @@
+riTripBudgetUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+  
+  )
+}
+
+riTripBudgetServer <- function(id) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      
+    }
+  )
+}
